590e97dcbb43f4d1e93914713d7ff212423c97ec
hika019 <ikazuti2001@gmail.com>
2020-10-15 09:37:51 +0900

    なぞ

--- app/src/main/java/com/example/paint/MainActivity.kt
+++ app/src/main/java/com/example/paint/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.paint
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.SurfaceHolder
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +14,56 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         /// CustomSurfaceViewのインスタンスを生成しonTouchリスナーをセット
-        val customSurfaceView = paint(surfaceView)
+        val customSurfaceView = CustomSurfaceView(this, surfaceView)
         surfaceView.setOnTouchListener { v, event ->
             customSurfaceView.onTouch(event)
         }
+
+        /// カラーチェンジボタンにリスナーをセット
+        /// CustomSurfaceViewのchangeColorメソッドを呼び出す
+        black_btn.setOnClickListener {
+            customSurfaceView.changeColor("black")
+        }
+        red_btn.setOnClickListener {
+            customSurfaceView.changeColor("red")
+        }
+        blue_btn.setOnClickListener {
+            customSurfaceView.changeColor("green")
+        }
+
     }
+
+
+    /// surfaceViewが作られたとき
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        /// bitmap,canvas初期化
+        initializeBitmap()
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        /// bitmapをリサイクル
+        prevBitmap!!.recycle()
+    }
+
+
+    /// bitmapとcanvasの初期化
+    private fun initializeBitmap() {
+        if (prevBitmap == null) {
+            prevBitmap = Bitmap.createBitmap(width!!, height!!, Bitmap.Config.ARGB_8888)
+        }
+
+        if (prevCanvas == null) {
+            prevCanvas = Canvas(prevBitmap!!)
+        }
+
+        prevCanvas!!.drawColor(0, PorterDuff.Mode.CLEAR)
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/paint/paint.kt
+++ app/src/main/java/com/example/paint/paint.kt
@@ -1,117 +1,156 @@
-package com.example.paint
-
-import android.content.Context
-import android.graphics.*
-import android.util.Log
-import android.view.*
-
-class paint (CustomSurfaceView: SurfaceView){
-    private var surfaceHolder: SurfaceHolder? = null
-    private var paint: Paint? = null
-    private var path: Path? = null
-    var color: Int? = null
-    var prevBitmap: Bitmap? = null
-    private var prevCanvas: Canvas? = null
-    private var canvas: Canvas? = null
-
-    var width: Int? = null
-    var height: Int? = null
-
-
-    private fun draw(pathInfo: pathInfo) {
-        canvas = Canvas()
-
-        /// ロックしてキャンバスを取得
-        canvas = surfaceHolder!!.lockCanvas()
-
-        //// キャンバスのクリア
-        canvas!!.drawColor(0, PorterDuff.Mode.CLEAR)
-
-        /// 前回のビットマップをキャンバスに描画
-        canvas!!.drawBitmap(prevBitmap!!, 0F, 0F, null)
-
-        //// pathを描画
-        paint!!.color = pathInfo.color
-        canvas!!.drawPath(pathInfo.path, paint!!)
-
-        /// ロックを解除
-        surfaceHolder!!.unlockCanvasAndPost(canvas)
-    }
-
-
-
-//    @Override
-//    protected fun draw(pathInfo: pathInfo) {
-//        canvas = Canvas()
-//        canvas = surfaceHolder!!.lockCanvas()
+//package com.example.paint
+//
+//import android.content.Context
+//import android.graphics.*
+//import android.os.Build
+//import android.util.Log
+//import android.view.*
+//import androidx.annotation.RequiresApi
+//
+//
+//
+//class paint (CustomSurfaceView: SurfaceView, surfaceHolder: SurfaceHolder){
+//    private var surfaceHolder: SurfaceHolder? = null
+//    private var paint: Paint? = null
+//    private var path: Path? = null
+//    var color: Int? = null
+//    var prevBitmap: Bitmap? = null
+//    private var prevCanvas: Canvas? = null
+//    private var canvas: Canvas? = null
+//
+//    var width: Int? = null
+//    var height: Int? = null
+//
+//
+//    constructor(context: Context, surfaceView: SurfaceView){
+//        surfaceHolder = surfaceView.holder
+//
+//        /// display の情報（高さ 横）を取得
+//        val size = Point().also {
+//            (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.apply {
+//                getSize(
+//                    it
+//                )
+//            }
+//        }
+//
+//        /// surfaceViewのサイズ
+//        width = size.x
+//        height = size.y
+//
+//        /// 背景を透過させ、一番上に表示
+//        surfaceHolder!!.setFormat(PixelFormat.TRANSPARENT)
+//        surfaceView.setZOrderOnTop(true)
+//
+//        /// コールバック
+////        surfaceHolder!!.addCallback(this)
+//
+//        /// ペイント関連の設定
+//        paint = Paint()
+//        color = Color.BLACK
+//        paint!!.color = color as Int
+//        paint!!.style = Paint.Style.STROKE
+//        paint!!.strokeCap = Paint.Cap.ROUND
+//        paint!!.isAntiAlias = true
+//        paint!!.strokeWidth = 15F
+//    }
+//
+//
+//
+//    private fun draw(pathInfo: pathInfo) {
+////        canvas = Canvas()
+//        /// ロックしてキャンバスを取得
+//        val canvas = surfaceHolder!!.lockCanvas()
+//
+//        //// キャンバスのクリア
 //        canvas!!.drawColor(0, PorterDuff.Mode.CLEAR)
+//
+//        /// 前回のビットマップをキャンバスに描画
 //        canvas!!.drawBitmap(prevBitmap!!, 0F, 0F, null)
+//
+//        //// pathを描画
 //        paint!!.color = pathInfo.color
 //        canvas!!.drawPath(pathInfo.path, paint!!)
+//
+//        /// ロックを解除
 //        surfaceHolder!!.unlockCanvasAndPost(canvas)
 //    }
-
-    fun onTouch(event: MotionEvent): Boolean {
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> hoge(event.x, event.y)
-            MotionEvent.ACTION_MOVE -> Log.d("paint", "move ${event.x}, ${event.y}")
-            MotionEvent.ACTION_UP -> Log.d("paint", "up ${event.x}, ${event.y}")
-        }
-
-        return true
-    }
-
-//     path クラスで描画するポイントを保持
-//        ACTION_DOWN 時の処理
-//    private fun touchDown(x: Float, y: Float) {
-//        path = Path()
-//        path!!.moveTo(x, y)
+//
+//
+//    /// bitmapとcanvasの初期化
+//    private fun initializeBitmap() {
+//        if (prevBitmap == null) {
+//            prevBitmap = Bitmap.createBitmap(width!!, height!!, Bitmap.Config.ARGB_8888)
+//        }
+//
+//        if (prevCanvas == null) {
+//            prevCanvas = Canvas(prevBitmap!!)
+//        }
+//
+//        prevCanvas!!.drawColor(0, PorterDuff.Mode.CLEAR)
 //    }
 //
-//    ///    ACTION_MOVE 時の処理
-//    private fun touchMove(x: Float, y: Float) {
-//        /// pathクラスとdrawメソッドで線を書く
-//        path!!.lineTo(x, y)
-//        draw(pathInfo(path!!, color!!))
-//        prevCanvas!!.drawPath(path!!, paint!!)
 //
+//
+//
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    fun onTouch(event: MotionEvent): Boolean {
+//        if (prevBitmap == null){
+//            initializeBitmap()
+//        }
+//
+//        when (event.action) {
+//            MotionEvent.ACTION_DOWN -> Log.d("paint", "down ${event.x}, ${event.y}")
+//            MotionEvent.ACTION_MOVE -> hoge(event.x, event.y)
+//            MotionEvent.ACTION_UP -> Log.d("paint", "up ${event.x}, ${event.y}")
+//        }
+//
+//        return true
 //    }
 //
-//    ///    ACTION_UP 時の処理
-//    private fun touchUp(x: Float, y: Float) {
-//        /// pathクラスとdrawメソッドで線を書く
-//        path!!.lineTo(x, y)
-//        draw(pathInfo(path, color?)()
-//        /// 前回のキャンバスを描画
-//        prevCanvas!!.drawPath(path!!, paint!!)
+////     path クラスで描画するポイントを保持
+////        ACTION_DOWN 時の処理
+////    private fun touchDown(x: Float, y: Float) {
+////        path = Path()
+////        path!!.moveTo(x, y)
+////    }
+////
+////    ///    ACTION_MOVE 時の処理
+////    private fun touchMove(x: Float, y: Float) {
+////        /// pathクラスとdrawメソッドで線を書く
+////        path!!.lineTo(x, y)
+////        draw(pathInfo(path!!, color!!))
+////        prevCanvas!!.drawPath(path!!, paint!!)
+////
+////    }
+////
+////    ///    ACTION_UP 時の処理
+////    private fun touchUp(x: Float, y: Float) {
+////        /// pathクラスとdrawメソッドで線を書く
+////        path!!.lineTo(x, y)
+////        draw(pathInfo(path, color?)()
+////        /// 前回のキャンバスを描画
+////        prevCanvas!!.drawPath(path!!, paint!!)
+////    }
+//
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    private fun hoge(x: Float, y:Float){
+//        var canvas = surfaceHolder?.lockCanvas()
+//        //// キャンバスのクリア
+//        canvas?.drawColor(Color.BLACK)
+//        //長方形が描けるはず
+//        var paint = Paint()
+//        var lineStrokeWidth = 20F
+//        paint.color = Color.argb(255f, 255f, 0f, 255f)
+//
+//        paint.style = Paint.Style.STROKE
+//        canvas?.drawRect(300f, 300f, 600f, 600f, paint)
+//        surfaceHolder?.unlockCanvasAndPost(canvas)
 //    }
-
-    private fun hoge(x: Float, y:Float){
-        canvas = Canvas()
-        canvas = surfaceHolder?.lockCanvas()
-        //// キャンバスのクリア
-        val backgraund = Color.argb(0,0,0,0)
-
-        canvas?.drawColor(backgraund, PorterDuff.Mode.CLEAR) //ぬるぽ原因（多分背景）
-
-
-
-        var paint = Paint()
-        val lineStrokeWidth = 20F
-
-        paint!!.color = Color.argb(255, 255, 0 , 255)
-        paint!!.style = Paint.Style.STROKE
-
-        canvas?.drawRect(300f, 300f, 600f, 600f, paint)
-
-
-    }
-
-
-
-
-}
-
-//// pathクラスの情報とそのpathの色情報を保存する
-data class pathInfo(var path: Path, var color: Int) {
-}
+//}
+//
+//
+//
+////// pathクラスの情報とそのpathの色情報を保存する
+//data class pathInfo(var path: Path, var color: Int) {
+//}
