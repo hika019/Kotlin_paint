7215c592a9eaf2d4e0f55c7c6e0dd4e7e2b05b0c
hika019 <ikazuti2001@gmail.com>
2020-10-21 00:21:16 +0900

    色変更試みる

--- app/src/main/java/com/example/paint/MainActivity.kt
+++ app/src/main/java/com/example/paint/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.paint
 
-
+//https://qiita.com/InoueDaiki/items/3b51aaabdaad1c2227a3
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val paint_class = PaintView(this)
+
+
+        //色を変えれるか
+        var color_data = color("black")
+
+        black_btn.setOnClickListener {
+            color_data.color ="black"
+        }
+        red_btn.setOnClickListener {
+            Log.d("color", "onClick red_btn")
+            color_data.color ="red"
+            Log.d("color", "main color_data: ${color_data.color}")
+        }
     }
 
-
 }
+
+class color(var color:String)
--- app/src/main/java/com/example/paint/Paint.kt
+++ app/src/main/java/com/example/paint/Paint.kt
@@ -2,32 +2,49 @@ package com.example.paint
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
 
 class PaintView @JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null) :
     View(context, attrs) {
-    private val paint: Paint
+    val paint: Paint
     private val path: Path
+    var tmp: Int? = null
+    var color_data = color()
+
 
     init {
         path = Path()
         paint = Paint()
-//        paint.setColor(-0x1000000)
-        paint.setColor(-0x0FF0F0F)
+//        paint.setColor(Color.BLACK)
         paint.setStyle(Paint.Style.STROKE)
         paint.setStrokeJoin(Paint.Join.ROUND)
         paint.setStrokeCap(Paint.Cap.ROUND)
         paint.setStrokeWidth(10F)
+
+        Log.d("color", "init:${paint.color}")
     }
+
+
 
     @Override
     override fun onDraw(canvas: Canvas) {
+        //色部分追加
+        Log.d("color", "color_data: ${color_data.color}")
+        when(color_data.color){
+            "black" -> paint.setColor(Color.BLACK)
+            "red" -> paint.setColor(Color.RED)
+            "blue" -> tmp = Color.BLUE
+        }
+
         canvas.drawPath(path, paint)
+        Log.d("color", "drow")
     }
 
     @Override
@@ -51,4 +68,24 @@ class PaintView @JvmOverloads constructor(context: Context?, attrs: AttributeSet
         return true
     }
 
+    fun clear() {
+        path.reset()
+        invalidate()
+    }
+
+}
+
+class hoge(){
+    var paint: Paint? = null
+    var tmp:Int? = null
+    fun change_color(color: String){
+        when (color){
+            "black" -> tmp = Color.BLACK
+            "red" -> tmp = Color.RED
+            "blue" -> tmp = Color.BLUE
+        }
+        Log.d("color", "${tmp}")
+        paint?.color = tmp as Int
+
+    }
 }
